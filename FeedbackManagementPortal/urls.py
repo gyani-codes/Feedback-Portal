5ee99891dfd0570bf8c3e08a6aba727116756921
Vishal-codes <icm2017005@iiita.ac.in>
2020-07-31 09:47:17 +0530

    url's added

--- FeedbackManagementPortal/urls.py
+++ FeedbackManagementPortal/urls.py
@@ -15,7 +15,41 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from feedbackApp import views
 urlpatterns = [
+    # redirect to admin
     url(r'^admin/', admin.site.urls),
+
+    # home page
+    url(r'^$',views.HomePage,name='Home'),
+
+    #if return dashboard for student
+    url(r'^dashboard_student/$',views.dashboard_student,name = "dashboard_student"),
+
+    # if reuqest for teacher return dashboard of teacher
+    url(r'^dashboard_teacher/$',views.dashboard_teacher,name = "dashboard_teacher"),
+
+    # feedback form
+    url(r'^form/$',views.feedback_form,name = "feedback_form"),
+
+    # logout
+    url(r'^logout/$', views.logout_view,name='logout'),
+    
+    # teahcer detail
+    url(r'^teacher/$', views.teachers_detail, name='teachers_detail'),
+
+    # techer detail in student dashboard , slug is fac code for teacher
+    url(r'^teacher/(?P<slug>[-\w\d]+)$', views.teachers_detail2, name='teachers_detail2'),
+
+    # techer detail in teacher dashboard , slug is fac code for teacher
+
+    url(r'^teacher2/(?P<slug>[-\w\d]+)$', views.teachers_detail3, name='teachers_detail3'),
+
+    # feedback detail in student dashboard 
+
+    url(r'^view_feedback/$', views.view_feedback, name='view_feedback'),
+
+    # feedback detail in student dashboard 
+
+    url(r'^view_feedback2/$', views.view_feedback2, name='view_feedback2'),
 ]
